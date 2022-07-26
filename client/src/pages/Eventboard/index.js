bcc2bdc04a2467b4d31ccfedcb77b84985ca8f02
alexmovact <alexander@movact.de>
2022-07-26 14:44:54 +0200

    add get event data

--- client/src/pages/Eventboard/index.js
+++ client/src/pages/Eventboard/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Eventboard() {
-  return <h1>Hello Eventboard</h1>;
+  const { id } = useParams();
+
+  const [event, setEvent] = useState(null);
+
+  const getOneEvent = () => {
+    const storedToken = localStorage.getItem("authToken");
+    axios
+      .get(`http://localhost:5005/api/events/${id}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setEvent(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getOneEvent();
+  }, []);
+
+  return (
+    <>
+      {event === null ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div key={event.id}>
+          <h1>{event.title}</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Eventboard;
